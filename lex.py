b445dd31fb25c70f9ac49a6b85e8e72172b204b7
Phil Yeeles <phil@yeel.es>
2022-06-27 17:46:48 +0100

    greatly improved lexer using developments from pyalog

--- lex.py
+++ lex.py
@@ -4,49 +4,70 @@ class KLex(Lexer):
     # K base verbs: +-*%|&^!<>=~@?_,#$.:
     # K I/O verbs: 0: 1: 2: 3: 4: 5: 6:
     # K base adverbs: / \ ' /: \: ':
-    tokens = { NAME, NUMBER, FLOAT, SYMBOL, CHARACTER, WSPACE,    # Datatypes
-               PLUS, MINUS, STAR, PERCENT, PIPE, AMPERSAND, CARET, # Verbs
-               BANG, LTHAN, GTHAN, EQUALS, TILDE, AT, QUESTION,
-               UNDERSCORE, COMMA, HASH, DOLLAR, DOT, COLON, SPACE,
-               FSLASH, BSLASH, TICK, FSCOL, BSCOL, TICKCOL,        # Adverbs
-               SCOLON, NEWLINE, BRACKL, BRACKR, SBRACKL, SBRACKR,  # Other
-               BRACEL, BRACER }
+    tokens = { NUMBER, CHAR, STRING, SYMBOL, ID, LPAREN, RPAREN, LBRACK, RBRACK, LBRACE, RBRACE, SPACE, PFUNC, COLON, SEMI }
 
+    # String containing ignored characters between tokens
     ignore_comment_line = r'^\/.*$'
     ignore_comment_inline = r'\s+\/.*$'
 
-    # ignore = r'$\s+' # Ignore leading whitespace # TODO: This + trailing
+    # Regular expression rules for tokens
 
-    # Tokens
-    NAME = r'[a-zA-Z][a-zA-Z0-9]*' # TODO: Escape dots?
-    SYMBOL = r'`"?[a-zA-Z][a-zA-Z0-9]"?' # TODO: Ensure 2 or no quotes
-    CHARACTER = r'".*"' # TODO: Ensure 2 or no quotes - probably better as a rule rather than in this regexp
-    WSPACE = r'\s+'
-    V=r'([-:+*%!&|<>=~,^#_$?@.]|0:|1:)'
-    A=r'(\':|/:|\\:|[\\\'/])'
-    N=r'`?"?[a-zA-z][a-zA-Z0-9*]"?'
-
-    # Other
-    SCOLON = r';'
-    NEWLINE = r'\n'
-    BRACKL = r'\('
-    BRACKR = r'\)'
-    SBRACKL = r'\['
-    SBRACKR = r'\]'
-    BRACEL = r'\{'
-    BRACER = r'\}'
-
-    @_(r'\d*\.?\d+') # TODO: E notation? 0x? 0b? negs not needed as handled by monadic -:
+    @_(r'(m)?\d*\.?\d+(J(m)?\d*\.?\d+)?')  # FIXME: m replaces overbar for ease of testing
     def NUMBER(self,t):
-        try:
-            float(t.value)
-            return t
-        except:
-            int(t.value)
-            return t
+        t.value = t.value.replace('m','-') # FIXME: m replaces overbar for ease of testing
+        if 'J' in t.value:
+            t.value = t.value.split('J')
+            for i in range(len(t.value)):
+                if "." in t.value[i]:
+                    t.value[i] = float(t.value[i])
+                else:
+                    t.value[i] = int(t.value[i])
+            t.value = complex(t.value[0],t.value[1])
+        elif '.' in t.value:
+            t.value = float(t.value)
+        else:
+            t.value = int(t.value)
+        return t
+        # TODO: implement 0x, 0b, E notation - are these already handled by python anyway?
+
+    @_(r'\'.\'')
+    def CHAR(self,t):
+        t.value=t.value[1:-1]
+        return t
+
+    @_(r'\'((\\\')|[^\'(\\\')])+\'')
+    def STRING(self,t):
+        t.value=t.value[1:-1]
+        return t
+    # TODO: arrayify into array of CHARs
+
+    @_(r'`\.?[a-zA-Z]+[a-zA-Z0-9\.]*\b')
+    def SYMBOL(self,t):
+        t.value=t.value[1::]
+        return t
+
+    ID      = r'\.?[a-zA-Z]+[a-zA-Z0-9\.]*'
+    LPAREN  = r'\('
+    RPAREN  = r'\)'
+    LBRACK = r'\['
+    RBRACK = r'\]'
+    LBRACE = r'\{'
+    RBRACE = r'\}'
+    SPACE = r'[\ \t]+'
+    # TODO: implement characters and strings ("/')
+
+    # K primitive functions
+    PFUNC = r'[+\-]'
+
+    # K primitive operators
+    # TODO
+
+    # Other K symbols
+    COLON = r':'
+    SEMI = r'[;\n]'
 
 if __name__ == '__main__':
-    d = 'x:2;foo:3.45;x+foo /this is a comment' # test with inline comment
+    d = 'x:2;foo:3.45;x+foo;`sym /this is a comment' # test with inline comment
     c = '/this is another comment'              # test with line comment
     l = KLex()
     for t in l.tokenize(d):
