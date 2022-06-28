a376652cdd00d034a874770353f62f43b091e68f
Phil Yeeles <phil@yeel.es>
2022-06-28 09:29:32 +0100

    migrated to based array model, lists; parser now able to interpret atoms and arrays of atoms

--- parse.py
+++ parse.py
@@ -1,24 +1,45 @@
+import ast
+from astor import to_source
 from sly import Parser
 from lex import KLex
 
 class KParse(Parser):
     tokens = KLex.tokens
 
-    @_('expr PLUS term')
+    @_('array SEMI')
     def expr(self,p):
-        return p.expr + p.term
+        return ast.fix_missing_locations(ast.Expression(p.array))
 
-    @_('term')
+    @_('atom SEMI')
     def expr(self,p):
-        return p.term
+        return ast.fix_missing_locations(ast.Expression(p.atom))
 
-    @_('factor')
-    def term(self,p):
-        return p.factor
+    @_('array SPACE array')
+    def array(self,p):
+        return ast.List(elts=(p.array0.elts + p.array1.elts),ctx=ast.Load())
+
+    @_('atom SPACE array')
+    def array(self,p):
+        return ast.List(elts=([p.atom] + p.array.elts),ctx=ast.Load())
+
+    @_('array SPACE atom')
+    def array(self,p):
+        return ast.List(elts=(p.array.elts + [p.atom]),ctx=ast.Load())
+
+    @_('atom SPACE atom')
+    def array(self,p):
+        return ast.List(elts=[p.atom0,p.atom1],ctx=ast.Load())
 
     @_('NUMBER')
-    def factor(self,p):
-        return p.NUMBER
+    def atom(self,p):
+        return ast.Constant(p.NUMBER)
+
+    @_('CHAR')
+    def atom(self,p):
+        if p.CHAR=='q':
+            exit(0)
+        else:
+            return ast.Constant(p.CHAR)
 
 if __name__ == '__main__':
     l = KLex()
@@ -26,8 +47,11 @@ if __name__ == '__main__':
 
     while True:
         try:
-            t = input('  ')
-            r = p.parse(l.tokenize(t))
-            print(r)
+            text = input('apl > ')
+            result = p.parse(l.tokenize(text+'\n'))
+            print("      "+to_source(result))
+            result = eval(compile(result, filename="<ast>", mode="eval"))
+            print("      ==> "+str(result)+": "+str(type(result))+"\n")
+            #print(result)
         except EOFError:
             break
